92d7d13f0f2aeb9b2a17756de08a9e58b1109508
Joey Kanaly <me@joeykanaly.com>
2020-11-12 19:23:22 -0600

    Added FAQ Section Content

---
+++ src/components/FAQSection.js
@@ -0,0 +1,41 @@
+export default function Faqsection(props) {
+	return (
+		<div className='faq'>
+			<h2>
+				Any Questions <span>FAQ</span>
+			</h2>
+			<div className='question'>
+				<h4>How Do I Start?</h4>
+				<p>Lorem ipsum dolor sit amet.</p>
+				<p>
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo,
+					mollitia.
+				</p>
+			</div>
+			<div className='question'>
+				<h4>Daily Schedule</h4>
+				<p>Lorem ipsum dolor sit amet.</p>
+				<p>
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo,
+					mollitia.
+				</p>
+			</div>
+			<div className='question'>
+				<h4>Diffrent Payment Methods</h4>
+				<p>Lorem ipsum dolor sit amet.</p>
+				<p>
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo,
+					mollitia.
+				</p>
+			</div>
+			<div className='question'>
+				<h4>What Products Do You Offer?</h4>
+				<p>Lorem ipsum dolor sit amet.</p>
+				<p>
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo,
+					mollitia.
+				</p>
+			</div>
+		</div>
+	);
+}
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,10 +1,12 @@
 import AboutSection from '../components/AboutSection';
 import ServicesSection from '../components/ServicesSection';
+import FAQSection from '../components/FAQSection';
 const AboutUs = () => {
 	return (
 		<>
 			<AboutSection />
 			<ServicesSection />
+			<FAQSection />
 		</>
 	);
 };
